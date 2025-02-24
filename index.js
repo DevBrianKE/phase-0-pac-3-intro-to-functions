36490d3c4219724be9f8c6d9751f8f38eb9404c9
DevBrianKE <kipchumbabrian47@gmail.com>
2025-02-24 14:41:25 +0300

    practice

--- index.js
+++ index.js
@@ -1,1 +1,123 @@
-// Follow along with the examples here
+// // Follow along with the examples here
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+// console.log("Hello, world!");
+
+
+// function sayHello() {
+//     console.log("Hello!")
+// }
+
+// sayHello()
+
+// function sayHelloToGuadalupe() {
+//     console.log("Hello, Guadalupe!")
+// }
+
+
+// function sayHelloToLiz() {
+//     console.log("Hello, Liz!");
+//   }
+
+
+
+//   function sayHelloToSamip() {
+//     console.log("Hello, Samip!");
+//   }
+
+//   sayHelloToGuadalupe();
+//   sayHelloToLiz();
+//   sayHelloToSamip();
+
+
+
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}`)
+// }
+
+// sayHelloTo("Guadalupe")
+// sayHelloTo("Jane")
+// sayHelloTo("R2-D2");
+
+
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+
+// say("Goodbye", "Julio");
+// say("Julio", "hello")
+
+// function say(greeting, firstName) {
+//     console.log("firstName: ", firstName);
+//     console.log("greeting: ", greeting);
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+
+// say("Goodbye", "Julio");
+
+// const greet = function() {
+//     console.log("Hello World")
+// }
+
+// greet()
+
+// const greet = function sayHello() {
+//     console.log("Hello");
+// }
+// greet()
+
+
+// const functionsArray = [
+//     function() { return "Hello"; },
+//     function() { return "World"; },
+//     function() { return "!"; }
+// ];
+
+// console.log(functionsArray[0]()); // Output: Hello
+// console.log(functionsArray[1]()); // Output: World
+// console.log(functionsArray[2]()); // Output: !
+
+// const mathOperations = {
+//     add: function(a, b) {return a + b },
+//     subtract: function(a, b) { return a - b}
+// }
+// console.log(mathOperations.add(5,3))
+
+// function say({ greeting, firstName }) {
+//   console.log("firstName: ", firstName);
+//   console.log("greeting: ", greeting);
+//   console.log(`${greeting}, ${firstName}!`);
+// }
+
+// Calling the function
+// say({ firstName: "Julio", greeting: "Hello" });
+
+// function say(greeting = "hello", firstName = "stranger") {
+  
+//   console.log(`${greeting}, ${firstName}!`);
+// }
+
+// say()
+
+// function say(greeting, firstName) {
+//   console.log(`${greeting}, ${firstName}!`);
+// }
+
+// say("Hello", "Liz")
+
+// 
+
+// function add(x, y) {
+//   return x + y;
+// }
+
+// console.log(add(5, 3))
